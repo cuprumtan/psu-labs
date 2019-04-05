75ff7cb7547804e3d29aed1c5782bd490d78e4f7
cuprumtan <cuprumtan@gmail.com>
2019-04-05 18:51:57 +0500

    modified zypper variable

--- install_packages.sh
+++ install_packages.sh
@@ -5,7 +5,7 @@ declare -A osInfo;
 osInfo[/etc/redhat-release]=yum
 osInfo[/etc/arch-release]=pacman
 osInfo[/etc/gentoo-release]=emerge
-osInfo[/etc/SuSE-release]=zypp
+osInfo[/etc/SuSE-release]=zypper
 osInfo[/etc/debian_version]=apt-get
 
 for f in ${!osInfo[@]}
