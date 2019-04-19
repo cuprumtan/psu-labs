bf405e0366fc6e54c86aefc368a62bc0e0868903
cuprumtan <cuprumtan@gmail.com>
2019-04-20 01:50:50 +0500

    Добавлен поиск серверов

--- chat_server/app.py
+++ chat_server/app.py
@@ -1,14 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Markup
 from flask_socketio import SocketIO, emit
+import json
+import subprocess
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'dder98342cuprumtan'
 socketio = SocketIO(app)
 
+def bash_get_servers():
+    result_array = []
+    proc = subprocess.Popen(["nmap -p 7071 192.168.0.* -oG - | grep 7071/open | awk '{print $2}'"], stdout=subprocess.PIPE, shell=True)
+    (stdout, err) = proc.communicate()
+    reader = stdout.decode('utf-8').splitlines()
+
+    for row in reader:
+        result_array.append(row)
+
+    return result_array
+
 @app.route('/')
 def index():
-    return render_template('./index.html')
+    #servers_array = ["192.168.0.5", "192.168.0.6"]
+    servers_array = bash_get_servers()
+    servers_array = json.dumps(servers_array)
+    return render_template('./index.html', servers_array=Markup(servers_array))
 
 def message_recived():
     print('message was received')
