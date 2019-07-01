37580f4d41a208f6852bd6fd71ed4df49642606a
Александра Кузнецова <adkuznetsova@lanit.ru>
2019-07-01 17:14:28 +0500

    Добавлена обработка недоступных страниц

--- app.py
+++ app.py
@@ -55,5 +55,15 @@ def manage_server():
     return render_template('server.html')
 
 
+@app.errorhandler(405)
+def page_not_found(e):
+    return render_template('404.html'), 405
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     app.run()
