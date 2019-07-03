3e99901992cb5b399c7ff56292c28fcdde3151b7
limonpudding <martyshenko.sergei@yandex.ru>
2019-07-03 10:27:45 +0500

    Заготовка для /advanced_result

--- app.py
+++ app.py
@@ -35,9 +35,6 @@ ip_v4 = get_ipv4()
 
 @app.route('/')
 def root():
-    session_number = db_session.query(func.coalesce(func.max(CeSessions.session_number), 0)).all()
-    db_session.commit()
-    session['number'] = tuple(session_number[0])[0]
     return redirect(url_for('test_begin'))
 
 
@@ -45,6 +42,9 @@ def root():
 def test_begin():
     if request.method == 'GET':
         session.clear()
+    session_number = db_session.query(func.coalesce(func.max(CeSessions.session_number), 0)).all()
+    db_session.commit()
+    session['number'] = tuple(session_number[0])[0] + 1
     subjects_dict = {}
     subjects_data = db_session.query(CeSubjects.id, CeSubjects.subject_name).all()
     db_session.commit()
@@ -135,7 +135,7 @@ def test_result():
         questions_data = session.get('questions_data')
         answers_data = session.get('answers_data')
         session_date = datetime.datetime.now().strftime("%Y-%m-%d")# дата
-        session_number = session.get('session_number')
+        session_number = session.get('number')
         fio = session.get('fio')# фио студента
         group = session.get('group')# группа
         course = session.get('course')# курс
@@ -155,6 +155,18 @@ def test_result():
                             right_answered = right_answered + 1
                     else:
                         checked = False
+                    to_insert = CeSessions(
+                        session_number=session_number,
+                        session_date=session_date,
+                        student_name=fio,
+                        student_group=group,
+                        student_grade=course,
+                        answer_id=a_object[0],
+                        is_right=a_object[3]
+                    )
+                    db_session.add(to_insert)
+                    db_session.flush()
+                    db_session.commit()
                     # тут сохраняем
             if right_count == right_answered:
                 score = score + 1
@@ -168,6 +180,9 @@ def test_result():
 def manage_server():
     # p = subprocess.Popen("bash get_my_ip.sh", stdout=subprocess.PIPE, shell=True)
     # (output, err) = p.communicate()
+    if request.method == 'GET':
+        date_from = datetime.datetime.now().strftime("%Y-%m-%d")
+        date_to = datetime.datetime.now().strftime("%Y-%m-%d")
     if request.method == 'POST':
         date_from = request.form['date_from']
         date_to = request.form['date_to']
@@ -181,12 +196,66 @@ def manage_server():
             session['group'] = group
         else:
             session.clear()
-    return render_template('server.html', ip_list=ip_v4)
+            date_from = datetime.datetime.now().strftime("%Y-%m-%d")
+            date_to = datetime.datetime.now().strftime("%Y-%m-%d")
+    sessions_data = db_session.query(CeSessions.session_number,
+                                     CeSessions.session_date,
+                                     CeSessions.student_name,
+                                     CeSessions.student_group,
+                                     CeSessions.student_grade,
+                                     CeSessions.result_percent) \
+        .distinct(CeSessions.session_number) \
+        .filter(CeSessions.session_date >= date_from) \
+        .filter(CeSessions.session_date <= date_to) \
+        .order_by(CeSessions.session_number).all()
+    db_session.commit()
+    return render_template('server.html', ip_list=ip_v4, sessions=sessions_data)
 
+class UserAnswer():
+    id = 0
+    question_id = 0
+    answer_text = 0
+    is_right = 0
 
-@app.route('/advanced_result')
+@app.route('/advanced_result', methods=['GET'])
 def advanced_result():
-    return render_template('advanced_results.html')
+    session_number = request.args.get('session_number')
+    session_data = db_session.query(CeSessions.id,
+                                    CeSessions.session_number,
+                                    CeSessions.session_date,
+                                    CeSessions.student_name,
+                                    CeSessions.student_group,
+                                    CeSessions.student_grade,
+                                    CeSessions.answer_id,
+                                    CeSessions.is_right)\
+        .filter(CeSessions.session_number == session_number).all()
+    db_session.commit()
+    fio = session_data[0].student_name
+    group = session_data[0].student_group
+    course = session_data[0].student_grade
+    date = session_data[0].session_date
+    answers_data = db_session.query(CeAnswers.id,
+                                    CeAnswers.answer_text,
+                                    CeAnswers.question_id,
+                                    CeAnswers.is_right)\
+        .filter(CeAnswers.id.in_([data.answer_id for data in session_data])).all()
+    db_session.commit()
+    questions_data = db_session.query(CeQuestions.id,
+                                      CeQuestions.question_text) \
+        .filter(CeQuestions.id.in_([answer.question_id for answer in answers_data])).all()
+    db_session.commit()
+    id = 0
+    questions_dict = {}
+    user_answers = []
+    for answer in answers_data:
+        for sess_answer in session_data:
+            if answer.id == sess_answer.answer_id:
+                answer = UserAnswer()
+    # for session in session_data:
+    #     CeAnswers
+    #     questions_dict[id] =
+    # questions_dict =
+    return render_template('advanced_results.html', fio=fio, group=group, course=course, date=date, data=questions_dict)
 
 
 @app.errorhandler(405)
