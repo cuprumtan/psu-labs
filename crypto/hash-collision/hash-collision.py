a32727ce7d8ae98fa6627802acc181cdd3f0d1fe
cuprumtan <cuprumtan@gmail.com>
2019-09-27 18:08:31 +0500

    Законченная версия алгоритма для поиска количества коллизий

--- crypto/hash-collision/hash-collision.py
+++ crypto/hash-collision/hash-collision.py
@@ -1,14 +1,16 @@
 import hashlib
-
-# входные строки
-L = 'february'
-R = 'sandwich'
-
-# невидимый символ
-invisible_symbol = ' '
+import random
+import string
 
 
-def search_collision(L, R, size, invisible_symbol):
+# функция генерации байтовой строки
+def generate_string(length=8):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
+
+# функция поиска коллизии
+def search_collision(L, R, size, invisible_symbol=' '):
     counter = 1
     L_list = []
     R_list = []
@@ -16,9 +18,9 @@ def search_collision(L, R, size, invisible_symbol):
     L_hash = ''
     R_hash = ''
     while not flag:
-        L_hash = hashlib.md5(L.encode()).hexdigest()
+        L_hash = hashlib.sha256(L.encode()).hexdigest()
         L_list.append(L_hash[0:size])
-        R_hash = hashlib.md5(R.encode()).hexdigest()
+        R_hash = hashlib.sha256(R.encode()).hexdigest()
         R_list.append(R_hash[0:size])
         for i in L_list:
             for j in R_list:
@@ -27,9 +29,20 @@ def search_collision(L, R, size, invisible_symbol):
         L += invisible_symbol
         R += invisible_symbol
         counter += 1
-        print('Итерация {}'.format(counter))
     return counter
 
 
-number_of_iterations = search_collision(L, R, 2, invisible_symbol)
-print(number_of_iterations)
+# функция поиска среднего количества итераций
+def get_avg():
+    sum = 0
+    for i in range(5):
+        L = generate_string()
+        R = generate_string()
+        sum += search_collision(L, R, 2)
+    avg = sum / 5
+    return avg
+
+
+avg = get_avg()
+print('Хеш: md5; среднее количество итераций для получения коллизии II рода = ', avg)
+    
